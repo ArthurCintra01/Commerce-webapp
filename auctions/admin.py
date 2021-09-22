e11e2c6a17ace4e80c703da3e71654236a93f1e5
ArthurCintra01 <turcko.cintra@gmail.com>
2021-09-22 17:35:51 -0300

    commerce

--- auctions/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Listings, Comments, Bids
-# Register your models here.
-admin.site.register(Listings)
-admin.site.register(Comments)
-admin.site.register(Bids)
